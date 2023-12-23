2ee0ca3ae6e82a311b782d262702d6f846fbe50d
Rockymoto517 <Rockymoto517@gmail.com>
2023-12-23 02:24:32 -0600

    Fix keybind overlap

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<C-e>", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
-vim.keymap.set("n", "<C-h>", function()
+vim.keymap.set("n", "<C-m>", function()
 	harpoon:list():select(1)
 end)
 vim.keymap.set("n", "<C-t>", function()
