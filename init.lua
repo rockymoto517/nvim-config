64b3a0dc8b8fdf880786547429745051ff045071
Rockymoto517 <Rockymoto517@gmail.com>
2024-01-15 01:24:39 -0600

    Swap from packer to lazy

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+-- Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
