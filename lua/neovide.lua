b9b952e9561c411078cb17d3ad73802c632cd7ba
Rockymoto517 <Rockymoto517@gmail.com>
2024-05-10 19:27:04 -0500

    small neovide module

---
+++ lua/neovide.lua
@@ -0,0 +1,6 @@
+vim.o.guifont = "RobotoMono Nerd Font Mono"
+vim.g.neovide_window_blurred = false
+vim.g.neovide_transparency = 1.0
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_confirm_quit = true
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
